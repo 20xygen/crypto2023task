2490c475285380addaf3c675bf95f6c859829e5f
20xygen <71791668+20xygen@users.noreply.github.com>
2023-03-23 00:43:01 +0300

    Create test_creator.py

---
+++ test_creator.py
@@ -0,0 +1,26 @@
+f = 7895418
+p = 907
+real = 2 ** f
+sf = real % p
+n = real // p
+pn = p * n
+print(f, p, sf, n, pn)
+
+word = "xyz"
+oup = ''
+for i in range(len(word)):
+    c = ord(word[i])
+    c_bin = str(bin(c))[2:]
+    c_bin = "0" * (8 - len(c_bin)) + c_bin
+    oup += c_bin
+    print(c_bin, end='')
+print()
+print(int(oup, 2))
+
+cur_bin = oup
+# print(cur, cur_bin)
+for j in range(0, len(cur_bin), 8):
+    symbol = cur_bin[j: j+8]
+    # print(symbol)
+    print(chr(int(symbol, 2)), end='')
+print()
